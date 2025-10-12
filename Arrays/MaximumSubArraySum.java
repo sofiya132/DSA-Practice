3262adaa84eb66508edeaac54a19c58780ba572c
Sofiya Sindgi <sofiyasindgi@gmail.com>
2025-10-12 10:56:56 +0530

    Add solution for maximumsubarray sum(Kadane's approach)

---
+++ Arrays/MaximumSubArraySum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class MaximumSubArraySum {
+    public static int maxsubarrsum(int a[]) {
+        int maxsum = Integer.MIN_VALUE;
+        int cursum = 0;
+        int maxElement = a[0];
+        for(int i=0;i<a.length;i++){
+            cursum = cursum +a[i];
+            if(cursum<0){
+                cursum = 0;
+            }
+             maxsum = Math.max(maxsum,cursum);
+              maxElement = Math.max(maxElement,a[i]);
+        }
+      if(maxsum==0){
+          maxsum = maxElement;
+      }
+      return maxsum;  
+    }
+    
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]= s.nextInt();
+        }
+        System.out.print(maxsubarrsum(a));
+    }
+}
